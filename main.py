7c74fb97373b69bf522ddc07acf95394f4ce730c
Lazik <Lazik18@users.noreply.github.com>
2021-05-17 19:52:15 +0300

    commit

--- main.py
+++ main.py
@@ -1,75 +1,76 @@
-import pygame
+import sys
 import os
+import pygame
+import numpy as np
 
-# настройка папки ассетов
+pygame.init()
+
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, 'img')
-sound_folder = os.path.join(game_folder, 'sound')
-player_img = pygame.image.load(os.path.join(img_folder, 'cat.png'))
+
 map_img = pygame.image.load(os.path.join(img_folder, 'map.png'))
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+SIZE_CELL = 25
+SIZE_OBJ = 23
 
 
-class Map:
-    def __init__(self):
-        self.x = map_img.get_width()
-        self.y = map_img.get_height()
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = player_img
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+class Cat:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x + 1, y + 1, SIZE_OBJ, SIZE_OBJ)
 
     def update(self):
-        self.rect.x -= 3
-        if self.rect.right < 0:
-            self.rect.left = WIDTH
-            meow.play()
+        pygame.draw.rect(screen, (42, 141, 156), self.rect, 0)
 
 
-# Создаем игру и окно
-field = Map()
-pygame.init()
-pygame.mixer.init()
-meow = pygame.mixer.Sound(os.path.join(sound_folder, 'meow.mp3'))
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Evo")
-clock = pygame.time.Clock()
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
+class Wall:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x + 1, y + 1, SIZE_OBJ, SIZE_OBJ)
+
+    def update(self):
+        pygame.draw.rect(screen, (31, 52, 56), self.rect, 0)
 
 
-# Цикл игры
-running = True
-while running:
-    # Держим цикл на правильной скорости
-    clock.tick(FPS)
-    # Ввод процесса (события)
+class Food:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x + 1, y + 1, SIZE_OBJ, SIZE_OBJ)
+
+    def update(self):
+        pygame.draw.rect(screen, (230, 103, 97), self.rect, 0)
+
+
+screen = pygame.display.set_mode((700, 500))
+all_obj = []
+
+for i in range(0, map_img.get_width()):
+    for j in range(0, map_img.get_height()):
+        if map_img.get_at([i, j]) == pygame.color.Color(0, 0, 0):
+            obj = Wall(i * SIZE_CELL, j * SIZE_CELL)
+        elif map_img.get_at([i, j]) == pygame.color.Color(255, 0, 0):
+            obj = Food(i * SIZE_CELL, j * SIZE_CELL)
+        elif map_img.get_at([i, j]) == pygame.color.Color(0, 0, 255):
+            obj = Cat(i * SIZE_CELL, j * SIZE_CELL)
+        all_obj[i].append(obj)
+
+
+while True:
     for event in pygame.event.get():
-        # check for closing window
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
 
-    # Обновление
-    all_sprites.update()
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_LEFT:
+        #         rect.move_ip(-40, 0)
+        #     elif event.key == pygame.K_RIGHT:
+        #         rect.move_ip(40, 0)
+        #     elif event.key == pygame.K_UP:
+        #         rect.move_ip(0, -40)
+        #     elif event.key == pygame.K_DOWN:
+        #         rect.move_ip(0, 40)
 
-    # Рендеринг
-    screen.fill(BLUE)
-    all_sprites.draw(screen)
-    # После отрисовки всего, переворачиваем экран
+    screen.fill(pygame.color.Color(80, 80, 80))
+    for i in range(0, map_img.get_width()):
+        for j in range(0, map_img.get_height()):
+            all_obj[i][j].update()
+
     pygame.display.flip()
-
-pygame.quit()
